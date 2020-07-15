79f0669e9f17445873f20b3c14b25ca8f2088077
Ran05 <61574670+Ran05@users.noreply.github.com>
2020-07-15 19:44:01 +0800

    Update localScopeFunction.js

--- localScopeFunction.js
+++ localScopeFunction.js
@@ -11,3 +11,21 @@ myLocalScope();
 // Run and check the console
 // myVar is not defined outside of myLocalScope
 console.log('outside myLocalScope', myVar);
+
+
+//Basic JavaScript: Global vs. Local Scope in Functions
+
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+var outerWear  = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();

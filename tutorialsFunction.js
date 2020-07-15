7aad81aff432dd71a1cca2e35f886e1d32972376
Ran05 <61574670+Ran05@users.noreply.github.com>
2020-07-15 19:54:25 +0800

    Update tutorialsFunction.js

--- tutorialsFunction.js
+++ tutorialsFunction.js
@@ -10,9 +10,6 @@ var myVar = "forScope";
 }
 
 myLocalScope();
-
-// Run and check the console
-// myVar is not defined outside of myLocalScope
 console.log('outside myLocalScope', myVar);
 
 
@@ -23,11 +20,7 @@ console.log('outside myLocalScope', myVar);
 var outerWear = "T-Shirt";
 
 function myOutfit() {
-  // Only change code below this line
-
 var outerWear  = "sweater";
-
-  // Only change code above this line
   return outerWear;
 }
 

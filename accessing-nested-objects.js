3a313b9d5f446abfc05970baa447ba9e26ab10b8
Ran05 <61574670+Ran05@users.noreply.github.com>
2020-08-20 10:54:40 +0800

    Add files via upload

---
+++ accessing-nested-objects.js
@@ -0,0 +1,21 @@
+/**
+Basic JavaScript: Accessing Nested Objects
+The sub-properties of objects can be accessed 
+by chaining together the dot or bracket notation.
+ */
+
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
---
+++ manipulate-arrays-with-push.js
@@ -0,0 +1,15 @@
+/**
+Basic JavaScript: Manipulate Arrays
+ With push()Passed
+An easy way to append data
+ to the end of an array is via the push() function.
+
+.push() takes one or more parameters and 
+"pushes" them onto the end of the array.
+
+Examples:
+ */
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog",3]);

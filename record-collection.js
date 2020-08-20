67cf36931c437c4a4873d9a311a8a4f19a9edc0e
Ran05 <61574670+Ran05@users.noreply.github.com>
2020-08-20 19:59:25 +0800

    Add files via upload

---
+++ record-collection.js
@@ -0,0 +1,51 @@
+/**
+Basic JavaScript: Record Collection
+You are given a JSON object representing 
+a part of your musical album collection. 
+Each album has several properties and a 
+unique id number as its key. Not all albums 
+have complete information.
+ */
+
+
+// Setup
+var collection = {
+  2548: {
+    album: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: [
+      "Let It Rock",
+      "You Give Love a Bad Name"
+    ]
+  },
+  2468: {
+    album: "1999",
+    artist: "Prince",
+    tracks: [
+      "1999",
+      "Little Red Corvette"
+    ]
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [ ]
+  },
+  5439: {
+    album: "ABBA Gold"
+  }
+};
+
+// Only change code below this line
+function updateRecords(id, prop, value) {
+       if(value === "") delete collection[id][prop];
+  else if(prop === "tracks") {
+    collection[id][prop] = collection[id][prop] || [];
+    collection[id][prop].push(value);
+  } else {
+    collection[id][prop] = value;
+  }
+
+  return collection;
+}
+
+updateRecords(5439, "artist", "ABBA");

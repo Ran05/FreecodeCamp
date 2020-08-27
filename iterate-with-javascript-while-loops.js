63a1f9e7a9feb71bcc94504b523a175c5ca03494
Ran05 <61574670+Ran05@users.noreply.github.com>
2020-08-27 11:39:54 +0800

    Add files via upload

---
+++ iterate-with-javascript-while-loops.js
@@ -0,0 +1,24 @@
+/**
+Basic JavaScript: Iterate with JavaScript
+ While Loops
+You can run the same code multiple times
+ by using a loop.
+
+The first type of loop we will learn 
+is called a while loop because it runs 
+"while" a specified condition is true 
+and stops once that condition is no longer true.
+ */
+
+// Setup
+var myArray = [];
+
+  var i = 5;
+    while(i >= 0) {
+      myArray.push(i);
+     // myArray.reverse(i);
+      i--;
+    }
+     
+
+// Only change code below this line

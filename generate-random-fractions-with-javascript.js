71dc2c84aed7f7f2a07251b99320e0c64e8d733d
Ran05 <61574670+Ran05@users.noreply.github.com>
2020-09-12 19:15:37 +0800

    Add files via upload

---
+++ generate-random-fractions-with-javascript.js
@@ -0,0 +1,27 @@
+
+/**
+ Basic JavaScript: Generate 
+ Random Fractions with JavaScript
+Random numbers are useful for creating 
+random behavior.
+
+JavaScript has a Math.random() 
+function that generates a random 
+decimal number between 0 (inclusive) 
+and not quite up to 1 (exclusive). 
+Thus Math.random() can return a 0 
+but never quite return a 1
+
+Note
+ */
+
+
+
+
+function randomFraction(a) {
+
+    return Math.random();
+
+  
+}
+randomFraction(1);
